91e921956d124989c436a39deeb90bb14dbd692b
Carla022 <cs.morenol@outlook.com>
2022-08-11 23:27:13 +0100

    added code

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -1,8 +1,38 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class SecretNumber {
 
     public static void main(String[] args) {
 //        put code in here...
-        int secretNumber = 10;
-    }
+        int secretNumber = 4;
 
+        // Prompt a user to input their decision
+        System.out.println("Do You Want To Play A Game? (y/n)");
+
+        // Collect user input
+        Scanner myReader = new Scanner(System.in);
+        String userChoice = myReader.nextLine();
+
+        // if user choice is n, print 'Maybe Later Then :)'
+        if (userChoice.equals("n")) {
+            System.out.println("Maybe Later Then:)");
+        }
+
+        //if user input is y, begin game
+        if (userChoice.equals("y")) {
+            System.out.println("Lets Begin! Press \"Enter\"");
+            myReader.nextLine();
+        }
+//        Output the question
+        System.out.println("I'm Thinking of a Number 1 - 10. WHAT IS IT?🤔:");
+        int userGuess = Integer.parseInt(myReader.nextLine());   // user gives you a string, you want to be able to compare it with your secret number
+
+        if (userGuess==secretNumber) {
+            System.out.println("✨Well Done!✨(END)");
+        } else if( userGuess > secretNumber) {
+            System.out.println("too high");
+        } else System.out.println("too low");
+
+    }
 }
--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -1,7 +1,74 @@
+import java.util.Scanner;
+
 public class WhatToWear {
 
     public static void main(String[] args) {
-//        put code in here...
-    }
+        //        && (and) || (or)
+        //Prompt a user to input the weather (weather "rainy", "sunny", "cloudy", "snow")
+        System.out.println("Are Looking For: Accessory / Tops / Bottoms / Shoes?");
 
+        //8, 18, 28 degrees
+        //Collect user input
+        Scanner reader = new Scanner(System.in); // creates scanner object
+        String clothingArticle = reader.nextLine();
+
+        if (clothingArticle.equals("Accessory")) {
+            System.out.println("What is The Weather Like?");
+        }
+
+
+        String weather = reader.nextLine();
+
+        if (weather.equals("Sunny")) {
+            System.out.println("Sun Hat");
+        }
+        System.out.println("What Is The Temperature Like? Outside");
+
+//        int temperature = Integer.parseInt(reader.nextLine();
+        String userTemp = reader.nextLine();
+        if( userTemp > temperature ) {
+            System.out.println("Sun Hat");
+
+
+//        else {
+//            System.out.println("Brolly");
+//        }
+
+//        if weather is sunny + temp more than 15 degrees, print "Sun Hat"
+//        if weather is rainy, print "brolly"
+//
+//        System.out.println("Now: What Is The Temperature Like Outside?");
+//
+//
+//        if( userTemp > ) {
+//        System.out.println("too high");
+
+        }
+
+
+
+
+//        boolean Sunny = true; // ADDED
+//        boolean Rainy = false;  // ADDED
+//
+//        if (Sunny || Rainy){  // ADDED
+//            System.out.println("Sun Hat");
+
+
+
+
+
+
+//Accessory + Tops + Bottoms + Shoes
+
+
+//       Clothing:
+//        Sun hat + UV400 Sun glasses + Your favourite T-shirt + Shorts + Low-tops
+//        Raincoat + Brolly + Boots + lightweight Jumper + jeans
+//        Hoodie + Jeans + Trainers + Puffer Vest
+//        Jumper + Jacket + Scarf + Gloves + Winter Boots + Beanie
+
+
+
+    }
 }
